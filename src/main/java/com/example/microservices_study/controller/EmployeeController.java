af1894f096f08db37bd84cfe6335c948d28b25e4
Felipe Sato <felipegsato@gmail.com>
2026-05-24 23:48:00 -0300

    Implementing update,delete and findall methods

--- src/main/java/com/example/microservices_study/controller/EmployeeController.java
+++ src/main/java/com/example/microservices_study/controller/EmployeeController.java
@@ -11,6 +11,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.logging.Handler;
 
 @RestController
@@ -30,8 +31,25 @@ public class EmployeeController {
         //Ultiliza o ResponseEntitiiy como uma boa pratica para retornar o status correto
     }
 
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<EmployeeResponse> findAll(){
+        return employeeService.findAll();
+    }
+
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public Employee create(@RequestBody Employee employee){ //Transfroma o JSON que veio da resposta HTTP em um objeto java(employee.setName(...)
         return employeeService.create(employee);
+    }
+
+    @PutMapping(value = "{id}",  consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Employee update(@PathVariable("id") Long id, @RequestBody Employee employee){
+        return employeeService.update(employee);
+    }
+
+    @DeleteMapping(value = "{id}",  produces = MediaType.APPLICATION_JSON_VALUE)
+    public void delete(@PathVariable("id") Long id){
+        employeeService.delete(id);
     }
 }
--- src/main/java/com/example/microservices_study/service/EmployeeService.java
+++ src/main/java/com/example/microservices_study/service/EmployeeService.java
@@ -11,6 +11,9 @@ import org.springframework.boot.context.properties.PropertyMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class EmployeeService {
 
@@ -36,7 +39,7 @@ public class EmployeeService {
 //        employeeResponse.setBloodGroup(employee.getBloodGroup());
 
         //set data by making a rest api call
-        AddressResponse addressResponse  = restTemplate.getForObject("http://localhost:8083/address/{id}", AddressResponse.class, id);
+        AddressResponse addressResponse = restTemplate.getForObject("http://localhost:8083/address/{id}", AddressResponse.class, id);
 
         employeeResponse.setAddressResponse(addressResponse); // isso ainda esta null mas ja traz todos os atributos do AddresResponse, pois criei um atributo dentro do meu model, ai só estou preenchendo ele com o outro model
 
@@ -44,7 +47,42 @@ public class EmployeeService {
 
     }
 
+    // Sem DTO
+//    public List<Employee> findAll() {
+//        List<Employee> employee = employeeRepo.findAll();
+//        return employee;
+//    }
+
+    public List<EmployeeResponse> findAll() {
+        List<Employee> employees = employeeRepo.findAll();
+        List<EmployeeResponse> array = new ArrayList<EmployeeResponse>();
+        for (Employee employee : employees) {
+            EmployeeResponse employeeResponse = modelMapper.map(employee, EmployeeResponse.class);
+            array.add(employeeResponse);
+        }
+        return array;
+    }
+
+
     public Employee create(Employee employee) {
-         return employeeRepo.save(employee);
+        return employeeRepo.save(employee);
+    }
+
+    public Employee update(Employee employee) {
+        Employee entity = employeeRepo.findById(employee.getId()).get();
+
+        entity.setName(employee.getName());
+        entity.setEmail(employee.getEmail());
+        entity.setBloodGroup(employee.getBloodGroup());
+
+        return employeeRepo.save(entity);
+
+    }
+
+    public void delete(Long id) {
+        Employee entity = employeeRepo.findById(id).get();
+
+        employeeRepo.delete(entity);
     }
 }
+
